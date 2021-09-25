41c3b3009354cb6d409f2844e5721ee1bdc6f104
Jacko-2026 <90275948+Jacko-2026@users.noreply.github.com>
2021-09-25 10:07:00 -0400

    Exercise: Dodge-em (working on it)

--- exercises/dodge-em/js/script.js
+++ exercises/dodge-em/js/script.js
@@ -1,12 +1,35 @@
 /**
-Title of Project
-Author Name
+Exercise: Dodge-em
+Jack Taddeo
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Improvement/change ofthe COVID-19 “simulation” activity with a new interaction style, new visuals, and a new meaning
 */
 
 "use strict";
+
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0
+  }
+};
+
+let user = {
+  x: 250,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: 255
+};
 
 
 /**
@@ -21,7 +44,10 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth,windowHeight);
 
+  covid19.y = random(0,height);
+  covid19.vx = covid19.speed;
 }
 
 
@@ -29,5 +55,63 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+// Display Static
+  for (let i = 0; i < 1000; i++) {
+    let x = random(0,width);
+    let y = random(0,height);
+    stroke(255);
+    point(x,y);
+  }
+
+// Covid19 Movement
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0,height);
+  }
+
+// User Movement
+function handleInput() {
+  if (keyIsDown(LEFT_ARROW)) {
+    user.vx = -user.speed;
+  }
+  else if (keyIsDown(RIGHT_ARROW)) {
+    user.vx = user.speed;
+  }
+  else {
+    user.vx = 0
+  }
+  if (keyIsDown(UP_ARROW)) {
+    user.vy = -user.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    user.vy = user.speed;
+  }
+  else {
+    user.vy = 0;
+  }
+}
+
+function move() {
+  user.x = user.x + user.vx;
+  user.y = user.y + user.vy;
+}
+
+// Catching Covid 19
+  let d = dist(user.x,user.y,covid19.x,covid19.y);
+  if (d < covid19.size/2 + user.size/2) {
+    noLoop();
+  }
+
+// Display Covid 19
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  ellipse(covid19.x,covid19.y,covid19.size);
+
+// Display User
+  fill(user.fill);
+  ellipse(user.x,user.y,user.size);
 }
