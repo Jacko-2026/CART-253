11dbf7ff4f4cd65b648d0a0d6dac0f70310e3c45
Jacko-2026 <90275948+Jacko-2026@users.noreply.github.com>
2021-10-05 18:45:30 -0400

    Pixel Art-experiment

---
+++ experiments/pixel-art/js/script.js
@@ -0,0 +1,75 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+let menu = {
+  x:550,
+  y:10,
+  width: 140,
+  height: 680
+}
+
+let paintBrush = {
+  x:undefined,
+  y:undefined,
+  size:20
+}
+
+let redPaint = {
+  x:560,
+  y:20,
+  size: 20
+}
+let bluePaint = {
+
+}
+let greenPaint = {
+
+}
+let yellowPaint = {
+
+}
+let whitePaint = {
+
+}
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(700, 700);
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(0);
+
+  square(paintBrush.x,paintBrush.y,paintBrush.size);
+  paintBrush.x = mouseX;
+  paintBrush.y = mouseY;
+
+// Menu Bar for Paint Selection
+push();
+  fill(65);
+  strokeWeight(4);
+  stroke(51);
+  rect(menu.x, menu.y, menu.width, menu.height);
+pop();
+
+// Red Paint
+push();
+  fill(252, 23, 3);
+  noStroke(0);
+  square(redPaint.x, redPaint.y, redPaint.size);
+pop();
+}
