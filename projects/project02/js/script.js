e499e8202422960168479abfb2e7504f1e12ad10
Jacko-2026 <90275948+Jacko-2026@users.noreply.github.com>
2021-10-29 15:51:50 -0400

    02

--- projects/project02/js/script.js
+++ projects/project02/js/script.js
@@ -8,26 +8,90 @@ author, and this description to match your project!
 
 "use strict";
 
+let state = 'title';
 
-/**
-Description of preload
-*/
 function preload() {
 
 }
 
 
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(800, 800);
+  background(51);
 }
 
 
-/**
-Description of draw()
-*/
 function draw() {
 
+  // Screens/States
+  if (state === 'title') {
+    title();
+  }
+  else if (state === 'tutorial') {
+    tutorial();
+  }
+  else if (state === 'level1') {
+    level1();
+  }
+  else if (state === 'levelVic') {
+    levelVic();
+  }
+  else if (state === 'levelsMap') {
+    levelsMap();
+  }
+}
+
+// Levels +Title Screen & Victory Screens
+function title() {
+  push();
+  textSize(100);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text('START',width/2,height/2);
+  pop();
+}
+function tutorial() {
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text('TUTORIAL!',width/2,height/2);
+  pop();
+}
+function level1() {
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text('[BLANK]',width/2,height/2);
+  pop();
+}
+function levelVic() {
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text('LEVEL COMPLETE!',width/2,height/2);
+  pop();
+}
+function levelsMap() {
+  push();
+  textSize(64);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text('[BLANK]',width/2,height/2);
+  pop();
+}
+
+
+function mousePressed() {
+  if (state === 'title') {
+    state = 'tutorial';
+  }
+  if (state === 'tutorial') {
+    state = 'level1';
+  }
+  if (state === 'levelVic') {
+    state = 'levelsMap';
+  }
 }
